8add7c31a030b4c5d7ca35a8cc60aca98e50afff
Cid <stinermonza@gmail.com>
2024-03-23 00:13:58 -0300

    Model do professor iniciado

---
+++ backend/app/models/professor.py
@@ -0,0 +1,10 @@
+from .base import BaseModel
+from django.db import models
+from .usuario import Usuario
+
+class Professor(Usuario):
+
+    area = models.CharField(max_length=255)
+    grau_academico = models.CharField(max_length=255)
+    # TODO - Verificar se é melhor usar um campo de texto ou um campo de array
+    titulos = models.TextField()
