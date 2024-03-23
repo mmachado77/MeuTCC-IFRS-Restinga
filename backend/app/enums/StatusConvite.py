e1386b4c54024621620961a6e5a669623c949165
Matheus Machado <matheusmachado77@gmail.com>
2024-03-23 00:28:38 -0300

    Classe Convite Criada

--- backend/app/enums/StatusConvite.py
+++ backend/app/enums/StatusConvite.py
@@ -4,3 +4,7 @@ class StatusConvite(StrEnum):
     ANALISE = 'Convite em Análise'
     ACEITO = 'Convite Aceito'
     RECUSADO = 'Convite Recusado'
+
+    @classmethod
+    def choices(cls):
+        return [(key.value, key.name) for key in cls]
---
+++ backend/app/models/convite.py
@@ -0,0 +1,14 @@
+from .base import BaseModel
+from django.db import models
+from..enums import StatusConvite
+from .professor import Professor
+
+class Convite(BaseModel):
+    justificativa = models.CharField(verbose_name="Justificativa")
+    status = models.CharField(verbose_name = "Status", choices=StatusConvite.choices(), default=StatusConvite.ANALISE)
+    dataConvite = models.DateField.auto_now_add
+    dataStatus = models.DateTimeField.auto_now
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = False
