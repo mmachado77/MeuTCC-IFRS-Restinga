ea95efeb921aa81cc5fb4cd2b3b3f34f3b77b2ed
Matheus Costa <matheuscostakrenn@gmail.com>
2024-03-23 00:03:56 -0300

    Add enum grau academico

---
+++ backend/app/enums/GrauAcademico.py
@@ -0,0 +1,9 @@
+from enum import StrEnum
+
+class GrauAcademico(StrEnum):
+    TECNICO = 'Técnico'
+    GRADUACAO = 'Graduação'
+    POSGRADUACAO = 'Pós-Graduação'
+    MESTRADO = 'Mestrado'
+    DOUTORADO = 'Doutorado'
+    POSDOUTORADO = 'Pós-Doutorado'
