5097e29e9b9ba3bbaee8c1f364d7770785b8c0b4
Cid <stinermonza@gmail.com>
2024-03-23 00:28:14 -0300

    Model do professor externo inicializado

---
+++ backend/app/models/professorExterno.py
@@ -0,0 +1,16 @@
+from django.forms import DateTimeField
+from .base import BaseModel
+from django.db import models
+from .professor import Professor
+
+class ProfessorExterno(Professor):
+
+    # TODO - Verificar como será feito os campos de arquivo "identidade" e "diploma"
+    identidade = models.FileField(upload_to='professoresExterno/identidade')
+    diploma = models.FileField(upload_to='professoresExterno/diploma')
+
+    statusCadastro = models.BooleanField()
+    dataStatus = models.DateTimeField()
+
+class meta:
+    abstract = False
