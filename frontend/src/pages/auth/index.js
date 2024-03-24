928667c1c998228ac9de71dd3225741b34b64899
Matheus Costa <matheuscostakrenn@gmail.com>
2024-03-24 11:55:45 -0300

    Cria fluxo de login e gerar token

--- frontend/src/pages/auth/index.js
+++ frontend/src/pages/auth/index.js
@@ -3,14 +3,24 @@ import Image from 'next/image';
 import { Button } from 'primereact/button';
 import toast from 'react-hot-toast';
 import { useRouter } from 'next/router';
+import AuthService from 'meutcc/services/AuthService';
 
 const AuthPage = () => {
 
     const router = useRouter();
 
-    const handleClick = () => {
-        toast.loading('Autenticando...', { duration: 500 });
-        router.push('/submeter-proposta');
+    const handleLoginClick = async () => {
+        try {
+            const data = await toast.promise(AuthService.autenticar({ username: 'estudante@gmail.com' }), {
+                loading: 'Conectando...',
+                success: 'Conectado!',
+                error: 'Erro ao conectar',
+            });
+            localStorage.setItem('token', data.token);
+            router.push('/submeter-proposta');
+        } catch (error) {
+            console.error(error);
+        }
     }
 
     return <div className='max-w-screen-lg mx-auto bg-white m-3 mt-6 flex flex-col'>
@@ -24,7 +34,7 @@ const AuthPage = () => {
                 />
             </div>
             <div>
-                <Button onClick={handleClick} label="Entrar com o Google" icon="pi pi-google" className="p-button-raised p-button-rounded p-button-lg p-m-2" />
+                <Button onClick={handleLoginClick} label="Entrar com o Google" icon="pi pi-google" className="p-button-raised p-button-rounded p-button-lg p-m-2" />
             </div>
         </div>
     </div>;
