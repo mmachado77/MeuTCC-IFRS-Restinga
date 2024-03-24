b742e6980dcd4814752db98fa3e8118b89ad8a23
Matheus Costa <matheuscostakrenn@gmail.com>
2024-03-24 11:56:35 -0300

    Implementa o uso do axios nas services

---
+++ frontend/src/services/AuthService.js
@@ -0,0 +1,9 @@
+import { apiClient } from "meutcc/libs/api";
+
+async function autenticar(data) {
+    return apiClient.post('/app/autenticar/', data).then((response) => response.data);
+}
+
+export default {
+    autenticar,
+}
--- frontend/src/services/TccService.js
+++ frontend/src/services/TccService.js
@@ -1,12 +1,7 @@
+import { apiClient } from "meutcc/libs/api";
+
 async function submeterProposta(data) {
-    return fetch('http://localhost:8000/api/tccs/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${localStorage.getItem('token')}`,
-        },
-        body: JSON.stringify(data),
-    }).then((response) => response.json()).catch((error) => console.error(error));
+    return apiClient.post('/app/criar-tcc/', data).then((response) => response.data);
 }
 
 export default {
