ab22949586d39b492b248245985afc9589f21682
Matheus Machado <matheusmachado77@gmail.com>
2024-03-29 15:36:30 -0300

    criacao pagina cadastros-pendentes

--- frontend/src/pages/painel-configuracoes/index.js
+++ frontend/src/pages/painel-configuracoes/index.js
@@ -5,8 +5,7 @@ import Link from 'next/link';
 import ConfiguracoesService from 'meutcc/services/ConfiguracoesService';
 import { format } from 'date-fns';
 import { TabView, TabPanel } from 'primereact/tabview';
-
-
+import { Accordion, AccordionTab } from 'primereact/accordion';
 import { locale, addLocale, updateLocaleOption, updateLocaleOptions, localeOption, localeOptions } from 'primereact/api';
 import toast from 'react-hot-toast';
 
@@ -14,10 +13,14 @@ addLocale('ptbr', {
     today: 'Hoje', clear: 'Limpar', monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'], monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'], dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'], dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'], dayNamesMin: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'], weekHeader: 'Semana', firstDay: 0, isRTL: false, showMonthAfterYear: false, yearSuffix: '', timeOnlyTitle: 'Só Horas', timeText: 'Tempo', hourText: 'Hora', minuteText: 'Minuto', secondText: 'Segundo', ampm: false, month: 'Mês', week: 'Semana', day: 'Dia', allDayText: 'Todo o Dia'
 });
 
+
+
 const ConfiguracoesPage = () => {
     const [startDate, setStartDate] = useState(null);
     const [endDate, setEndDate] = useState(null);
 
+
+    
     const handleSaveDates = async () => {
         console.log('Data de Início:', startDate);
         console.log('Data de Fim:', endDate);
@@ -43,22 +46,6 @@ const ConfiguracoesPage = () => {
                 </div>
                 <div className="card">
                 <TabView>
-                    <TabPanel header="Propostas de TCC" leftIcon="pi pi-list mr-2">
-                        <p className="m-0">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        </p>
-                    </TabPanel>
-                    <TabPanel header="Permissões de Usuários" leftIcon="pi pi-users mr-2">
-                        <p className="m-0">
-                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-                            eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo
-                            enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui
-                            ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.
-                        </p>
-                    </TabPanel>
                     <TabPanel header="Prazo Envio de Propostas" leftIcon="pi pi-calendar mr-2" >
                     <div className="flex justify-between mb-5">
                                 <div>
