016c1ff127e547d1a10d58cfa909c70fc691a6ce
Matheus Machado <matheusmachado77@gmail.com>
2024-03-29 12:48:18 -0300

    Atualização de Prazo Proposta funciona na Header TAB

--- frontend/src/pages/painel-configuracoes/index.js
+++ frontend/src/pages/painel-configuracoes/index.js
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 import { Button } from 'primereact/button';
 import { Calendar } from 'primereact/calendar';
 import Link from 'next/link';
-import { Dialog } from 'primereact/dialog';
 import ConfiguracoesService from 'meutcc/services/ConfiguracoesService';
 import { format } from 'date-fns';
+import { TabView, TabPanel } from 'primereact/tabview';
+
 
 import { locale, addLocale, updateLocaleOption, updateLocaleOptions, localeOption, localeOptions } from 'primereact/api';
 import toast from 'react-hot-toast';
@@ -14,65 +15,83 @@ addLocale('ptbr', {
 });
 
 const ConfiguracoesPage = () => {
-    const [dialogVisible, setDialogVisible] = useState(false);
-    const [dates, setDates] = useState([]);
-
-    const handleOpenDialog = () => {
-        setDialogVisible(true);
-    };
-
-    const handleCloseDialog = () => {
-        setDialogVisible(false);
-    };
+    const [startDate, setStartDate] = useState(null);
+    const [endDate, setEndDate] = useState(null);
 
     const handleSaveDates = async () => {
-        console.log('Data de Início:', dates[0]);
-        console.log('Data de Fim:', dates[1]);
+        console.log('Data de Início:', startDate);
+        console.log('Data de Fim:', endDate);
         const datas = {
-            //format(currentDate, 'MMMM do, yyyy');
-            dataAberturaPrazoPropostas: format(dates[0], 'yyyy-MM-dd'),
-            dataFechamentoPrazoPropostas: format(dates[1], 'yyyy-MM-dd')
+            dataAberturaPrazoPropostas: format(startDate, 'yyyy-MM-dd'),
+            dataFechamentoPrazoPropostas: format(endDate, 'yyyy-MM-dd')
         }
-        setDialogVisible(false);
         const data = await toast.promise(ConfiguracoesService.atualizaDataProposta(datas), {
-            loading: 'Conectando...',
-            success: 'Conectado!',
-            error: 'Erro ao conectar',
+            loading: 'Atualizando...',
+            success: 'Prazo Alterado com Sucesso!',
+            error: 'Erro ao Atualizar',
         });
     };
 
     return (
-        <div className='max-w-screen-md mx-auto bg-white m-3 mt-6'>
-            <div className='py-3 border-0 border-b border-dashed border-gray-200'>
-                <h1 className='heading-1 text-center text-gray-700'>Configurações</h1>
-            </div>
 
-            <div className='py-6 px-9 grid grid-cols-3 gap-4'>
-                <Button icon="pi pi-calendar" label="Prazo Envio de Propostas" size="large" outlined onClick={handleOpenDialog} />
-                <Link href='/propostas-pendentes'>
-                    <Button icon="pi pi-list" label="Propostas de TCC" size="large" outlined />
-                </Link>
-                <Link href='/permissoes-usuarios'>
-                    <Button icon="pi pi-users" label="Permissões de Usuários" size="large" outlined />
-                </Link>
-            </div>
+        <div>
+            
 
-            <Dialog header="Envio de Propostas" visible={dialogVisible} style={{ width: 'fit-content' }} onHide={handleCloseDialog}>
-                <label>Período: </label>
-                <Calendar 
-                    value={dates} 
-                    onChange={(e) => setDates(e.value)} 
-                    dateFormat='dd/mm/yy' 
-                    locale='ptbr' 
-                    selectionMode="range" 
-                    readOnlyInput 
-                    hideOnRangeSelection 
-                    showButtonBar />
-                <div className="flex justify-between mt-5">
-                    <Button onClick={handleSaveDates} label="Salvar" severity="success" />
-                    <Button onClick={handleCloseDialog} label="Cancelar" severity="danger" />
+            <div className='max-w-screen-md mx-auto bg-white m-3 mt-6'>
+                <div className='py-3 border-0 border-b border-dashed border-gray-200'>
+                    <h1 className='heading-1 text-center text-gray-700'>Configurações</h1>
                 </div>
-            </Dialog>
+                <div className="card">
+                <TabView>
+                    <TabPanel header="Propostas de TCC" leftIcon="pi pi-list mr-2">
+                        <p className="m-0">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </TabPanel>
+                    <TabPanel header="Permissões de Usuários" leftIcon="pi pi-users mr-2">
+                        <p className="m-0">
+                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
+                            eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo
+                            enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui
+                            ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.
+                        </p>
+                    </TabPanel>
+                    <TabPanel header="Prazo Envio de Propostas" leftIcon="pi pi-calendar mr-2" >
+                    <div className="flex justify-between mb-5">
+                                <div>
+                                    <label>Data de Abertura do Prazo:</label>
+                                    <Calendar
+                                        value={startDate}
+                                        onChange={(e) => setStartDate(e.value)}
+                                        dateFormat='dd/mm/yy'
+                                        locale='ptbr'
+                                        readOnlyInput
+                                        showButtonBar
+                                    />
+                                </div>
+                                <div>
+                                    <label>Data de Fechamento do Prazo:</label>
+                                    <Calendar
+                                        value={endDate}
+                                        onChange={(e) => setEndDate(e.value)}
+                                        dateFormat='dd/mm/yy'
+                                        locale='ptbr'
+                                        readOnlyInput
+                                        showButtonBar
+                                    />
+                                </div>
+                            </div>
+                                <Button label="Enviar" severity="success" onClick={handleSaveDates} />
+                    </TabPanel>
+                </TabView>
+            </div>
+
+
+                
+            </div>
         </div>
     );
 };
