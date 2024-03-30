0ba62cdd9fd90ce87315dde12f240b3b5a68999d
Matheus Machado <matheusmachado77@gmail.com>
2024-03-29 23:56:05 -0300

    atualizar-permissoes lista corretamente e exibe detalhes de Cadastros de Professores ainda não aprovados

--- frontend/src/pages/atualizar-permissoes/listaProfessoresPendentes.js
+++ frontend/src/pages/atualizar-permissoes/listaProfessoresPendentes.js
@@ -5,10 +5,12 @@ import { Toast } from 'primereact/toast';
 import { Button } from 'primereact/button';
 import { Dialog } from 'primereact/dialog';
 import ProfessorService from 'meutcc/services/ProfessorService'; 
+import { format } from 'date-fns';
 
 export default function ProfessorsDemo() {
     let emptyProfessor = {
         id: null,
+        tipo: '',
         nome: '',
         cpf: '',
         email: '',
@@ -21,11 +23,6 @@ export default function ProfessorsDemo() {
         resourcetype: ''
     };
 
-    const mapaTipoRegistro = {
-        ProfessorInterno: 'Professor Interno',
-        ProfessorExterno: 'Professor Externo'
-    }
-
     const [professors, setProfessors] = useState(null);
     const [professorDialog, setProfessorDialog] = useState(false);
     const [professor, setProfessor] = useState(emptyProfessor);
@@ -36,13 +33,8 @@ export default function ProfessorsDemo() {
         async function fetchProfessors() {
             try {
                 const professoresPendentes = await ProfessorService.getProfessoresPendentes();
-                const professoresPendentes2 = professoresPendentes.map(professorPendente => {
-                    return {
-                        ...professorPendente, 
-                        resourcetype: mapaTipoRegistro[professorPendente.resourcetype]
-                    }
-                })
-                setProfessors(professoresPendentes2);
+                
+                setProfessors(professoresPendentes);
             } catch (error) {
                 console.error('Erro ao obter professores pendentes:', error);
                 // Exiba uma mensagem de erro se necessário
@@ -63,9 +55,7 @@ export default function ProfessorsDemo() {
 
     const actionBodyTemplate = (rowData) => {
         return (
-            <React.Fragment>
-                <Button label="Detalhes" severity="info" onClick={() => detalhesProfessor(rowData)} />
-            </React.Fragment>
+                <Button label="Detalhes" icon='pi pi-search-plus' severity="secondary" onClick={() => detalhesProfessor(rowData)} />
         );
     };
 
@@ -76,51 +66,53 @@ export default function ProfessorsDemo() {
                 <DataTable value={professors} selection={selectedProfessor} onSelectionChange={(e) => setSelectedProfessor(e.value)} dataKey="id" paginator rows={10} rowsPerPageOptions={[5, 10, 25]} currentPageReportTemplate="Showing {first} to {last} of {totalRecords} professors">
                     <Column field="nome" header="Nome" sortable></Column>
                     <Column field="email" header="Email" sortable></Column>
-                    <Column field="resourcetype" header="Tipo de Registro" sortable style={{ width: '12rem' }}></Column>
+                    <Column field="tipo" header="Tipo de Registro" sortable style={{ width: '12rem' }}></Column>
                     <Column body={actionBodyTemplate} exportable={false} style={{ minWidth: '8rem' }}></Column>
                 </DataTable>
             </div>
 
             <Dialog visible={professorDialog} style={{ width: '32rem' }} header="Detalhes do Cadastro" modal className="p-fluid" onHide={hideDialog}>
                 <div style={{ marginBottom: '1rem' }}>
-                    <label htmlFor="nome" className="font-bold">Nome:</label>
+                    <label htmlFor="nome" className="font-bold">Nome: </label>
                     <span>{professor.nome}</span>
                 </div>
                 <div style={{ marginBottom: '1rem' }}>
-                    <label htmlFor="cpf" className="font-bold">CPF:</label>
+                    <label htmlFor="cpf" className="font-bold">CPF: </label>
                     <span>{professor.cpf}</span>
                 </div>
                 <div style={{ marginBottom: '1rem' }}>
-                    <label htmlFor="email" className="font-bold">Email:</label>
+                    <label htmlFor="email" className="font-bold">Email: </label>
                     <span>{professor.email}</span>
                 </div>
                 <div style={{ marginBottom: '1rem' }}>
-                    <label htmlFor="dataCadastro" className="font-bold">Data de Cadastro:</label>
-                    <span>{professor.dataCadastro}</span>
+                    <label htmlFor="dataCadastro" className="font-bold">Data de Cadastro: </label>
+                    <span>{format(professor.dataCadastro, 'dd/MM/yyyy')}</span>
                 </div>
                 <div style={{ marginBottom: '1rem' }}>
-                    <label htmlFor="area" className="font-bold">Área:</label>
+                    <label htmlFor="area" className="font-bold">Área: </label>
                     <span>{professor.area}</span>
                 </div>
                 <div style={{ marginBottom: '1rem' }}>
-                    <label htmlFor="grauAcademico" className="font-bold">Grau Acadêmico:</label>
+                    <label htmlFor="grauAcademico" className="font-bold">Grau Acadêmico: </label>
                     <span>{professor.grau_academico}</span>
                 </div>
-                <div style={{ marginBottom: '1rem' }}>
-                    <label htmlFor="matricula" className="font-bold">Matrícula:</label>
-                    <span>{professor.matricula}</span>
-                </div>
-                {professor.resourcetype === 'Professor Externo' && (
-                    <React.Fragment>
+                {professor.resourcetype === 'ProfessorInterno' && (
+                    <div style={{ marginBottom: '1rem' }}>
+                        <label htmlFor="matricula" className="font-bold">Matrícula: </label>
+                        <span>{professor.matricula}</span>
+                    </div>
+                )}
+                {professor.resourcetype === 'ProfessorExterno' && (
+                    <>
                         <div style={{ marginBottom: '1rem' }}>
-                            <label htmlFor="identidade" className="font-bold">Identidade:</label>
+                            <label htmlFor="identidade" className="font-bold">Identidade: </label>
                             <span>{professor.identidade || 'Não disponível'}</span>
                         </div>
                         <div style={{ marginBottom: '1rem' }}>
-                            <label htmlFor="diploma" className="font-bold">Diploma:</label>
+                            <label htmlFor="diploma" className="font-bold">Diploma: </label>
                             <span>{professor.diploma || 'Não disponível'}</span>
                         </div>
-                    </React.Fragment>
+                    </>
                 )}
             </Dialog>
         </div>
