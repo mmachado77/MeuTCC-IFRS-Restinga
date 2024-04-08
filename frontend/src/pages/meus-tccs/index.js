c59e63c39171235a0b691337e7f8306af6e1dc87
Cid <stinermonza@gmail.com>
2024-04-08 06:33:54 -0300

    Status do tcc no datatable

--- frontend/src/pages/meus-tccs/index.js
+++ frontend/src/pages/meus-tccs/index.js
@@ -6,12 +6,12 @@ import { FilterMatchMode } from 'primereact/api';
 import { Button } from 'primereact/button';
 import { GUARDS } from 'meutcc/core/constants';
 import { set } from 'date-fns';
+import { Tag } from 'primereact/tag';
 
 import React from 'react';
 import TccService from 'meutcc/services/TccService';
 import Link from 'next/link';
 import LoadingSpinner from 'meutcc/components/ui/LoadingSpinner';
-
 
 const MeusTccsPage = () => {
 
@@ -92,6 +92,30 @@ const MeusTccsPage = () => {
         return rowData.coorientador && rowData.coorientador.nome || 'Sem coorientador';
     }
 
+    const getClassForStatus = (status) => {
+        switch (status) {
+          case 'PROPOSTA_ANALISE_PROFESSOR':
+          case 'PROPOSTA_ANALISE_COORDENADOR':
+          case 'DESENVOLVIMENTO':
+          case 'PREVIA':
+          case 'FINAL':
+          case 'AJUSTE':
+            return '#FFBF00';
+          case 'PROPOSTA_RECUSADA_PROFESSOR':
+          case 'PROPOSTA_RECUSADA_COORDENADOR':
+          case 'REPROVADO_PREVIA':
+          case 'REPROVADO_FINAL':
+            return '#D2222D';
+          case 'APROVADO':
+            return '#007000';
+          default:
+        }
+      };
+
+    const statusBodyTemplate = (rowData) => {
+        return <Tag value={rowData?.status?.[rowData.status.length - 1]?.statusMensagem} style={{ backgroundColor: getClassForStatus(rowData?.status?.[rowData.status.length - 1]?.status)}}></Tag>
+    }
+
     // Place holder para eventos de expansão de linhas
     const onRowToggle = (e) => {
         setExpandedRows(e.data);
@@ -131,16 +155,17 @@ const MeusTccsPage = () => {
                 {/*<DataTable value={tccs} header={renderHeader} emptyMessage="Nenhum tema encontrado" filters={filters} paginator rows={5} tableStyle={{ minWidth: '50rem' }}>*/}
                 <DataTable value={tccs} expandedRows={expandedRows} onRowToggle={(e) => setExpandedRows(e.data)}
                 onRowExpand={onRowExpand} onRowCollapse={onRowCollapse} rowExpansionTemplate={rowExpansionTemplate}
-                dataKey="id" header={renderHeader} tableStyle={{ minWidth: '60rem' }}>
-                    
+                dataKey="id" header={renderHeader} tableStyle={{ minWidth: '50rem' }} emptyMessage="Nenhum tema encontrado" filters={filters} paginator rows={5}>
                     <Column field="tema" header="Título" style={{ width: '80%' }}></Column>
                     <Column field="orientador.nome" header="Orientador" style={{ width: '20%' }}></Column>
                     <Column body={coorientadorTemplate} header="Coorientador" style={{ width: '20%' }}></Column>
+                    <Column body={statusBodyTemplate} header="Status" style={{ width: '10%' }}></Column>
                     <Column body={actionBodyTemplate} exportable={false} style={{ minWidth: '8rem' }}></Column>
                     <Column expander={allowExpansion} style={{ width: '5rem' }} />
                 </DataTable>
             </div>
         </div>;
+        
     }else{
         return <div className='max-w-screen-lg mx-auto bg-white m-3 mt-6 flex flex-col'>
             <div className='py-3 border-0 border-b border-dashed border-gray-200'>
@@ -156,7 +181,6 @@ const MeusTccsPage = () => {
             </div>
         </div>;
     }
-
 }
 
 MeusTccsPage.guards = [GUARDS.ESTUDANTE, GUARDS.PROFESSOR_INTERNO, GUARDS.PROFESSOR_EXTERNO, GUARDS.COORDENADOR];
