f55faa4ed2cddd8a731f962f34a35e893865860f
Cid <stinermonza@gmail.com>
2024-04-07 21:13:38 -0300

    att model convite

--- backend/app/models/convite.py
+++ backend/app/models/convite.py
@@ -3,11 +3,11 @@ from django.db import models
 from . import Professor, Tcc
 
 class Convite(BaseModel):
+    aceito = models.BooleanField(default=False)
+    justificativaRecusa = models.TextField(blank=True, null=True)
+    dataConvite = models.DateTimeField(auto_now_add=True)
     professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
     tcc = models.ForeignKey(Tcc, on_delete=models.CASCADE)
-    aceito = models.BooleanField(default=False)
-    justificativa_recusa = models.TextField(blank=True, null=True)
-    data_convite = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         unique_together = ('professor', 'tcc')
