e02177c0e72891d2f6cb8b8d6e4f8b7d8e337dac
Cid <stinermonza@gmail.com>
2024-04-07 22:28:59 -0300

    tcc service "possuiProposta" criado

--- backend/app/services/tcc.py
+++ backend/app/services/tcc.py
@@ -1,8 +1,9 @@
 from app.models import TccStatus
 from app.enums import StatusTccEnum
-from backend.app.models.convite import Convite
-from backend.app.models.semestre import Semestre
-from backend.app.models.tcc import Tcc
+from app.models.convite import Convite
+from app.models.semestre import Semestre
+from app.models.tcc import Tcc
+from django.db.models import Max, F
 
 class TccService:
 
@@ -26,6 +27,21 @@ class TccService:
             Convite.objects.create(tcc=tcc, professor=tcc.coorientador)
 
         TccStatus.objects.create(tcc=tcc, status=StatusTccEnum.PROPOSTA_ANALISE_PROFESSOR)
+
+    def possuiProposta(self, usuario):
+        tccs = Tcc.objects.filter(autor=usuario)
+        if(tccs.exists()):
+            tccsCancelados = tccs.all().annotate(max_id=Max('tccstatus__id')).filter(
+            tccstatus__id=F('max_id'), 
+            tccstatus__status__in=StatusTccEnum.statusTccCancelado())
+           
+            if tccsCancelados.count() == tccs.count():
+                return True
+            else:
+                return False
+            
+        return True 
+
     
 
 
