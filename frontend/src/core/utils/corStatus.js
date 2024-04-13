bacd3d84ebe94cf4914a5ec19db96d6fb1f697ae
Cid <stinermonza@gmail.com>
2024-04-13 20:18:00 -0300

    Adição do status "desenvolvimento" com a cor Azul

--- frontend/src/core/utils/corStatus.js
+++ frontend/src/core/utils/corStatus.js
@@ -2,11 +2,12 @@ const getClassForStatus = (status) => {
     switch (status) {
       case 'PROPOSTA_ANALISE_PROFESSOR':
       case 'PROPOSTA_ANALISE_COORDENADOR':
+        return {status: 'Análise', cor: '#FFBF00'};
       case 'DESENVOLVIMENTO':
       case 'PREVIA':
       case 'FINAL':
       case 'AJUSTE':
-        return {status: 'Análise', cor: '#FFBF00'};
+        return {status: 'Desenvolvimento', cor: '#3b82f6'};
       case 'PROPOSTA_RECUSADA_PROFESSOR':
       case 'PROPOSTA_RECUSADA_COORDENADOR':
       case 'REPROVADO_PREVIA':
