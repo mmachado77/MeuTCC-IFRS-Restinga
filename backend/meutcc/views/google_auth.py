1c5020c550619d5836593b08bf8d89a74b91043a
Matheus Costa <matheuscostakrenn@gmail.com>
2024-04-14 15:48:50 -0300

    Adiciona tratativa para caso de erro

--- backend/meutcc/views/google_auth.py
+++ backend/meutcc/views/google_auth.py
@@ -12,22 +12,28 @@ class GoogleAuthView(APIView):
     googleAuthService = GoogleAuthService()
 
     def get(self, request):
-        return self.googleAuthService.request_authorization(request)
+        try:
+            return self.googleAuthService.request_authorization(request)
+        except Exception:
+            return redirect(settings.AUTH_ERROR_FRONTEND_URL)
 
 class GoogleAuthCallbackView(APIView):
     googleAuthService = GoogleAuthService()
 
     def get(self, request):
-        user_info = self.googleAuthService.request_callback(request)
+        try:
+            user_info = self.googleAuthService.request_callback(request)
 
-        user = User.objects.filter(username=user_info['id']).first()
+            user = User.objects.filter(username=user_info['id']).first()
 
-        encoded_data = b64encode(json.dumps(user_info).encode()).decode('utf-8')
+            encoded_data = b64encode(json.dumps(user_info).encode()).decode('utf-8')
 
-        if user is None:
-            user = User.objects.create_user(username=user_info['id'], email=user_info['email'], password=None)
-            user.save()
+            if user is None:
+                user = User.objects.create_user(username=user_info['id'], email=user_info['email'], password=None)
+                user.save()
 
-        token, created = Token.objects.get_or_create(user=user)
+            token, created = Token.objects.get_or_create(user=user)
 
-        return redirect(settings.AUTH_FRONTEND_URL.format(token = token.key, data = str(encoded_data)))
+            return redirect(settings.AUTH_FRONTEND_URL.format(token = token.key, data = str(encoded_data)))
+        except Exception:
+            return redirect(settings.AUTH_ERROR_FRONTEND_URL)
