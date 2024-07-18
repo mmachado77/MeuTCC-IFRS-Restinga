f7b8db5f67550cd9ea05fc2e2f8b27fe58542502
Cid <stinermonza@gmail.com>
2024-07-18 08:10:43 -0300

    model Tema atualizado

--- backend/app/models/tema.py
+++ backend/app/models/tema.py
@@ -6,7 +6,5 @@ class Tema(BaseModel):
     titulo = models.CharField(max_length=255)
     descricao = models.TextField(max_length=500)
     professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
-    # Campo que determina se a proposta está vinculada com algum aluno ou não (disponibilidade)
-    estudante = models.ForeignKey(Estudante, on_delete=models.SET_NULL, null=True, blank=True)
     class meta:
         abstract = False
