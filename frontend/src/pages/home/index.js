53d68df5c9bc3c5ad22f6e420353c6b0b1d0f1a8
Matheus Costa <matheuscostakrenn@gmail.com>
2024-07-25 00:18:41 -0300

    alterado para proximas defesas e biblioteca

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -26,7 +26,7 @@ export default function Page({ sessoes, tccs }) {
 
     return (<>
         <div className='flex flex-col max-w-screen-lg m-3 mx-auto mt-6 bg-white border border-solid rounded-md border-1 border-slate-200'>
-            <h1 className='px-5 pt-2 text-2xl font-medium'>Futuros trabalhos</h1>
+            <h1 className='px-5 pt-2 text-2xl font-medium'>Próximas defesas</h1>
             <div className='flex flex-row justify-around gap-5 px-5 pt-3 pb-6'>
                 <div>
                     <Calendar value={scheduleDate} sessoes={sessoes || []} onMonthChange={handleScheduleMonthChange} />
@@ -39,7 +39,7 @@ export default function Page({ sessoes, tccs }) {
         </div>
 
         <div className='flex flex-col max-w-screen-lg m-3 mx-auto mt-6 bg-white border border-solid rounded-md border-1 border-slate-200'>
-            <h1 className='px-5 pt-2 text-2xl font-medium'>Trabalhos passados</h1>
+            <h1 className='px-5 pt-2 text-2xl font-medium'>Biblioteca</h1>
             <div className='p-5'>
                 <Table tccs={tccs} />
             </div>
