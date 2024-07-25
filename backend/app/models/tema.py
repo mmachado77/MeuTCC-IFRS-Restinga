07b63f2bbcb51353d715e9b09b07366fbb0a509c
Cid <stinermonza@gmail.com>
2024-07-25 08:29:10 -0300

    atributo da model tema agora é usuário ao invés de professor

--- backend/app/models/tema.py
+++ backend/app/models/tema.py
@@ -1,10 +1,10 @@
 from .base import BaseModel
 from django.db import models
-from . import Professor, Estudante
+from . import Professor, Estudante, Usuario
 
 class Tema(BaseModel):
     titulo = models.CharField(max_length=255)
     descricao = models.TextField(max_length=500)
-    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    professor = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     class meta:
         abstract = False
