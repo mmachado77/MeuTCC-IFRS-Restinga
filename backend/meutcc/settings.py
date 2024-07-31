d89cd96a4db096338cfe56dfd1c3083107baef07
Cid <stinermonza@gmail.com>
2024-07-31 19:57:56 -0300

    adição do novo handler em settings

--- backend/meutcc/settings.py
+++ backend/meutcc/settings.py
@@ -150,7 +150,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'EXCEPTION_HANDLER': 'app.utils.custom_exception_handler.custom_exception_handler',
 }
 
 if DEBUG:
