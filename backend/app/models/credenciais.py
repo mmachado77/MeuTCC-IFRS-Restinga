bdb114d21b0054ce409ec53cf330d6c169aa1440
Matheus Costa <matheuscostakrenn@gmail.com>
2024-08-01 00:33:18 -0300

    altera model credenciais

--- backend/app/models/credenciais.py
+++ backend/app/models/credenciais.py
@@ -1,11 +1,8 @@
 from .base import BaseModel
 from django.db import models
 
-class Credenciais(BaseModel):
-    
+class Credenciais(BaseModel):    
     access_token = models.TextField()
-    refresh_token = models.TextField()
-    expires_in = models.DateTimeField()
     
     class Meta:
         abstract = False
